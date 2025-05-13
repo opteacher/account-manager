17a34e7fd48df99f8292c37ae0ed860107f0afc5
opteacher <opteacher@hotmail.com>
2025-05-13 17:42:15 +0800

    关闭WebView标签的警告

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => ['webview'].includes(tag)
+        }
+      }
+    }),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
